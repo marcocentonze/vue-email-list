cc042d995ac78e4fd1c1db34a295a30b946227e0
marcocentonze <marco.centonze27@gmail.com>
2023-09-20 16:44:06 +0200

    add loop for and v-for to print 10 random emails

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,19 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      randomEmail: [],
+      randomEmails: [], 
     };
   },
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        console.log(response);
-        this.randomEmail = response.data.response;
-      });
-  },
+    //ciclo for 
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          console.log(response);
+          //pusho per aggiungere la nuova email all'array
+          this.randomEmails.push(response.data.response); 
+        });
+    }
+  }
 }).mount("#app");
